7d59fb114bbdd125d948d1d715d0ac4eb84b9897
Evan <suzukaze.haduki@gmail.com>
2026-09-03 10:09:34 +0900

    fix(test): preserve Windows key type contract

--- tests/src/test_platform_api.cpp
+++ tests/src/test_platform_api.cpp
@@ -3,9 +3,16 @@
 #include <cstdint>
 #include <type_traits>
 
+#ifdef _WIN32
+static_assert(std::is_same_v<decltype(KeyStroke::vk), UINT>);
+static_assert(std::is_same_v<decltype(KeyStroke::modifiers_down), UINT>);
+static_assert(std::is_same_v<decltype(KeyStroke::wch), WCHAR>);
+static_assert(sizeof(WCHAR) == 2);
+#else
 static_assert(std::is_same_v<decltype(KeyStroke::vk), std::uint32_t>);
 static_assert(std::is_same_v<decltype(KeyStroke::modifiers_down), std::uint32_t>);
 static_assert(std::is_same_v<decltype(KeyStroke::wch), char16_t>);
+#endif
 
 int main()
 {
